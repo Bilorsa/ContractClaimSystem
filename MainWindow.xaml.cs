ba03b6defe65b079c31f8b4ff3684c3fb153328e
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-09-09 13:22:44 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContractClaimSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SubmitClaim_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Claim Submitted (Prototype Only)");
+        }
+
+        private void UploadDocument_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Document Uploaded (Prototype Only)");
+        }
+
+        private void ApproveClaim_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Claim Approved (Prototype Only)");
+        }
+    }
+}
